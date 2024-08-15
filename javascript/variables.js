05ef52e364e944762de492535121be2a7a50ceed
raocaptmemrao@gmail.com <raocaptmemrao@gmail.com>
2024-08-15 13:38:40 +0500

    js

---
+++ javascript/variables.js
@@ -0,0 +1,21 @@
+const accountId = 14393 // const value can not change once written
+
+let accountEmail = "memona@gmail.com" // can not change....mostly used 
+
+var accountPvassword = "abc123" // anything can be change
+
+let accountState;
+
+
+
+console.log(accountId, accountEmail, accountPvassword);
+
+// accountId = 2   // not allowed
+accountEmail = "memrao@gmail.com"
+
+
+accountPvassword = "ASCASDC"
+accountCity = "Karachi"
+
+console.log(accountId);
+console.table([accountId, accountPvassword, accountEmail, accountCity, accountState]) // table form
